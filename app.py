024a9889501c050be9d85c42e4ec0fcce163d288
NunoCos <nuno18_9@hotmail.com>
2021-04-24 10:39:27 +0000

    add task buttons

--- app.py
+++ app.py
@@ -107,14 +107,14 @@ def add_task():
             "category_name": request.form.get("category_name"),
             "task_name": request.form.get("task_name"),
             "task_description": request.form.get("task_description"),
-            "is urgent": is_urgent,
+            "is_urgent": is_urgent,
             "due_date": request.form.get("due_date"),
             "created_by": session["user"]
         }
         mongo.db.tasks.insert_one(task)
-        flash("Task Succefully Added")
+        flash("Task Successfully Added")
         return redirect(url_for("get_tasks"))
-        
+
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("add_task.html", categories=categories)
 
